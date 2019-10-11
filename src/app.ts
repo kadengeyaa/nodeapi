e259ebf360859021bea7e2986b4e3c136c492cd3
kadengeyaa <kadengeyaa@gmail.com>
2019-10-11 14:46:16 +0300

    Setup server

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,25 @@
 import 'reflect-metadata';
+import express from 'express';
 
-function logType(target: any, key: string): void {
-  const t = Reflect.getMetadata('design:type', target, key);
-  console.log(`${key} type: ${t.name}`);
+process.on('uncaughtException', (error: Error) => {
+  console.log('uncaughtException', error);
+
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason: {} | null | undefined, promise: Promise<any>) => {
+  console.log('unhandledRejection: Reason', reason);
+  console.log('unhandledRejection: Promise', promise);
+});
+
+async function startServer(): Promise<void> {
+  const app = express();
+
+  app.listen(9099, (error: Error) => {
+    if (error) throw error;
+
+    console.log('Server started');
+  });
 }
 
-function logParamTypes(target: any, key: string): void {
-  const types = Reflect.getMetadata('design:paramtypes', target, key);
-  const s = types.map(a => a.name).join();
-  console.log(`${key} param types: ${s}`);
-}
-
-function logReturnType(target: any, key): void {
-  const type = Reflect.getMetadata('design:returntype', target, key);
-
-  console.log(`${key} return type: ${type.name}`);
-}
-
-class Foo {}
-
-class Person {
-  @logType
-  public name: string;
-
-  @logReturnType
-  @logParamTypes
-  doSomething(
-    param1: string,
-    param2: number,
-    param3: Foo,
-    param4: { test: string },
-    param5: {},
-    param6: Function,
-    param7: (a: number) => void,
-  ): number {
-    return 1;
-  }
-}
+startServer();
