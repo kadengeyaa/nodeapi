da0cd12deaed7f5f93e884e3ae6a4f71e892d80c
kadengeyaa <kadengeyaa@gmail.com>
2019-10-11 12:24:42 +0300

    Class decorators

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,33 @@ function logFunction(_target: any, key: string, value: any): any {
   };
 }
 
+function logClass(target: any): void {
+  // save a reference to the original constructor
+  const original = target;
+
+  // a utility function to generate instances of a class
+  function construct(constructor, args): any {
+    const c: any = function() {
+      return constructor.apply(this, args);
+    };
+    c.prototype = constructor.prototype;
+    return new c();
+  }
+
+  // the new constructor behaviour
+  const f: any = function(...args) {
+    console.log('New: ' + original.name);
+    return construct(original, args);
+  };
+
+  // copy prototype so intanceof operator still works
+  f.prototype = original.prototype;
+
+  // return new constructor (will override original)
+  return f;
+}
+
+@logClass
 class Person {
   @logProperty
   public name: string;
