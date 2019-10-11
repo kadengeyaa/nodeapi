cf502a3157171503903a8411439f87eb649393bd
kadengeyaa <kadengeyaa@gmail.com>
2019-10-11 17:07:13 +0300

    TOFIX: Express returns  html

--- src/loader/express.ts
+++ src/loader/express.ts
@@ -1,4 +1,6 @@
-import { Application } from 'express';
+import { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import morganBody from 'morgan-body';
 
 export function initApp(app: Application): void {
   app.get('/status', (req, res) => {
@@ -7,5 +9,19 @@ export function initApp(app: Application): void {
 
   app.head('/status', (req, res) => {
     res.status(200).end();
+  });
+
+  app.enable('trust proxy');
+
+  app.use(bodyParser.json());
+
+  morganBody(app);
+
+  app.use((req, res, next) => {
+    next(new Error('URL not found'));
+  });
+
+  app.use((error: Error, req: Request, res: Response) => {
+    res.status(401).json({ error });
   });
 }
