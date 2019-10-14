78f06a1c365f3140e424fed179003b59b04639c3
kadengeyaa <kadengeyaa@gmail.com>
2019-10-14 18:44:35 +0300

    Added user modeling

---
+++ src/model/user.ts
@@ -0,0 +1,44 @@
+import { Schema, model, Document } from 'mongoose';
+
+const isName = (name: string): boolean => !!name.match(/^[^\W\d_]+$/);
+
+const isUsername = (username: string): boolean => !!username.match(/^[\w]+$/);
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate: {
+      validator: isName,
+    },
+  },
+  lastName: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate: {
+      validator: isName,
+    },
+  },
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: isUsername,
+    },
+  },
+  password: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  salt: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+});
+
+export const UserModel = model<User & Document>('User', userSchema);
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,11 @@ type DefaultError = Error & {
   code?: string;
   status?: number;
 };
+
+type User = {
+  firstName: string;
+  lastName: string;
+  username?: string;
+  password?: string;
+  salt?: string;
+};
