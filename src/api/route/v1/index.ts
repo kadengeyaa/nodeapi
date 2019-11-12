9b75eb41f38592222a12ac192573a6e1333c1887
kadengeyaa <kadengeyaa@gmail.com>
2019-11-12 15:51:31 +0300

    Added account router

--- src/api/route/v1/index.ts
+++ src/api/route/v1/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { authRouter } from './auth';
-import { userRouter } from './user';
+import { accountRouter } from './account';
 
 export function v1Router(): Router {
   const router = Router();
 
   router.use('/auth', authRouter());
-  router.use('/user', userRouter());
+  router.use('/account', accountRouter());
 
   return router;
 }
